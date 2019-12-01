c4a6202b5023889c3b9b04b6a38bdafd49a63b93
sjones97 <sjones97@email.unc.edu>
2019-12-01 16:27:29 -0500

    initial Model commit

--- src/a8/Model.java
+++ src/a8/Model.java
@@ -1,6 +1,27 @@
 package a8;
 
-import java.util.*;
+public class Model {
+    private int size;
+    private int[][] board;
 
-public class GameOfLifeModel {
+    // Movement directions
+    public enum Direction {
+        NORTH, SOUTH, EAST, WEST
+    }
+
+    // Setup new board
+    public Model(int size) {
+        this.size = size;
+        reset();
+    }
+
+    // Get size of the board
+    public int getSize() {
+        return size;
+    }
+
+    // Resets game
+    public void reset() {
+        board = new int[size][size];
+    }
 }
